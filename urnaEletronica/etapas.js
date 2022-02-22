3a66753ce3e821b1f80305ed662b0cc92b9c22f9
Thais Michelini <78885096+thaismichelini@users.noreply.github.com>
2022-02-21 23:00:20 -0300

    Urna Eletrônica

---
+++ urnaEletronica/etapas.js
@@ -0,0 +1,52 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: 38111,
+                nome: 'Fulano de Tal',
+                partido: 'ABC',
+                fotos: [
+                    {url: 'fulanoDeTal.png', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: 77222,
+                nome: 'Beltrano da Silva',
+                partido: 'DEFG',
+                fotos: [
+                    {url:'beltranoDeTal.png', legenda: 'Vereador'}
+                ] 
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: 99,
+                nome: 'Ciclano',
+                partido: 'ABC',
+                vice: 'Cic',
+                fotos: [
+                    {url: 'ciclano.png', legenda: 'Prefeito'},
+                    {url: 'cic.png', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: 84,
+                nome: 'Zulano',
+                partido: 'QUERTY',
+                vice: 'Zul',
+                fotos: [
+                    {url: 'zulano.png', legenda: 'Prefeito'},
+                    {url: 'zul.png', legenda: 'Vice-Prefeito', small: true}
+                ]        
+
+            },
+
+        ]
+    }
+]
