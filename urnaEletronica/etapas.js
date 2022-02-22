451820240ac4d8dde43ea08c8090cefc38278845
Thais Michelini <78885096+thaismichelini@users.noreply.github.com>
2022-02-22 11:42:44 -0300

    Continuação Urna

--- urnaEletronica/etapas.js
+++ urnaEletronica/etapas.js
@@ -4,7 +4,7 @@ let etapas = [
         numeros: 5,
         candidatos: [
             {
-                numero: 38111,
+                numero: '38111',
                 nome: 'Fulano de Tal',
                 partido: 'ABC',
                 fotos: [
@@ -12,7 +12,7 @@ let etapas = [
                 ]
             },
             {
-                numero: 77222,
+                numero: '77222',
                 nome: 'Beltrano da Silva',
                 partido: 'DEFG',
                 fotos: [
@@ -26,7 +26,7 @@ let etapas = [
         numeros: 2,
         candidatos: [
             {
-                numero: 99,
+                numero: '99',
                 nome: 'Ciclano',
                 partido: 'ABC',
                 vice: 'Cic',
@@ -36,7 +36,7 @@ let etapas = [
                 ]
             },
             {
-                numero: 84,
+                numero: '84',
                 nome: 'Zulano',
                 partido: 'QUERTY',
                 vice: 'Zul',
